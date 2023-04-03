ae3f4a95260e4b7b24c7052f1cc384eba1498d6d
kasvoy <woj.kasperczyk@gmail.com>
2023-04-03 12:01:27 +0400

    organized files

---
+++ security_groups.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "web_access" {
+  name        = "allow_web"
+  description = "Allow Web traffic"
+  vpc_id = aws_vpc.website_vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "asg_sg" {
+  name = "autoscaling"
+  vpc_id = aws_vpc.website_vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
-#variables to be added here
+variable "region" {
+  type        = string
+  description = "The AWS Region to use"
+
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "AWS VPC CIDR range"
+}
